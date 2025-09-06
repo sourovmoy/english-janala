d4b71a5ce24caceda8e6ad3d184194d27dbb6067
sourovmmoysanju <sourovmmoysanju@gmail.com>
2025-09-06 14:07:54 +0600

    add modal for word showing

--- script.js
+++ script.js
@@ -83,18 +83,76 @@ const wordsLoad = (words) => {
       }"</h4>
               </div>
               <div class="px-10 mt-10 flex justify-between">
-                <span class="p-3 bg-[#1a91ff1a] rounded-md"
+                <button onclick="loadModalWord(${
+                  word.id
+                })" class="p-3 bg-[#1a91ff1a] rounded-md"
                   ><i class="fa-solid fa-circle-question"></i
-                ></span>
-                <span class="p-3 bg-[#1a91ff1a] rounded-md"
+                ></button>
+                <button class="p-3 bg-[#1a91ff1a] rounded-md"
                   ><i class="fa-solid fa-volume-high"></i
-                ></span>
+                ></button>
               </div>
         </div>
       `;
       container.append(div);
     });
   }
+};
+// for modal words details wordsLoad
+const loadModalWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((word) => showWord(word.data));
+  const showModal = document.getElementById("show-modal");
+  showModal.innerHTML = "";
+  const showWord = (id) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="border-[#edf7ff] border-5 rounded-md p-5">
+                <h3 class="text-lg text-left font-bold">
+                ${
+                  id.word ? id.word : "শব্দ পাওয়া যাই নি"
+                }(<i class="fa-solid fa-microphone-lines"></i>: ${
+      id.pronunciation ? id.pronunciation : "উচ্চারণ পাওয়া যাই নি"
+    })
+              </h3>
+              <h5 class="text-left font-bold pt-4">Meaning</h5>
+              <h6 class="text-left p-2 font-medium">${
+                id.meaning ? id.meaning : "অর্থ পাওয়া যাই নি "
+              }</h6>
+              <h4 class="text-left font-bold pt-4 ">Example</h5>
+              <h6 class="text-left p-2 font-medium">
+                ${id.sentence ? id.sentence : "Sentence পাওয়া যাই নি"}
+              </h6>
+              <h4 class="text-left py-4 font-bold ">সমার্থক শব্দ গুলো</h5>
+             <div class="flex gap-4">
+               <p class=" bg-[#d7e4ef] rounded-md p-2 ">${
+                 id.synonyms[0] ? id.synonyms[0] : ""
+               }</p>
+               <p class=" bg-[#d7e4ef] rounded-md p-2">${
+                 id.synonyms[1] ? id.synonyms[1] : ""
+               }</p>
+               <p class=" bg-[#d7e4ef] rounded-md p-2">${
+                 id.synonyms[2] ? id.synonyms[2] : ""
+               }</p>
+            </div>
+            </div>
+            <div class="modal-action">
+                <form method="dialog">
+                  <!-- if there is a button in form, it will close the modal -->
+                  <button class="btn btn-primary rounded-xl">
+                    Complete Learning
+                  </button>
+                </form>
+              </div>
+    
+    `;
+    showModal.append(div);
+  };
+
+  // modal function call
+  my_modal_5.showModal();
 };
 
 getData(); //button call function
